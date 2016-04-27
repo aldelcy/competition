645239bfa107c4d943a5b17953cb6d21efb9a961
Al Delcy <aldelcy2000@gmail.com>
2016-04-27 19:29:21 -0400

    answer 1

---
+++ problem_1/multiple.rb
@@ -0,0 +1,20 @@
+class MultipleAdder
+
+	def sum_of_multiple(num)
+		total = 0
+		(0..num).each do |x|
+			
+			if x%3 == 0 || x%5 == 0			
+				total  = total + x
+			end		
+		end
+		puts total
+	end
+	
+end
+
+
+
+adder = MultipleAdder.new
+
+adder.sum_of_multiple(1000)
